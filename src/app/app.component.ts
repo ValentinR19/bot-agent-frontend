b733dc567167dc35ee42085eaaf8c99c6b72ec6b
Valentin <vrodriguez.crucijuegos@gmail.com>
2025-11-18 19:34:39 -0300

    Se corrigen ciertas librerías

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { PrimeNG } from 'primeng/config';
+import 'reflect-metadata';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'bot-agent-frontend';
+export class AppComponent implements OnInit {
+  private primeNG: PrimeNG = inject(PrimeNG);
+
+  ngOnInit(): void {
+    this.primeNG.ripple.set(true);
+    this.primeNG.zIndex = {
+      modal: 1100,
+      overlay: 1000,
+      menu: 1000,
+      tooltip: 1100,
+    };
+  }
 }
